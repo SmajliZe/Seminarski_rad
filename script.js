82a33d31689c4733a72d5ea9a6c7d2ab95f07560
SmajliZe <ismailbektas4@gmail.com>
2022-05-25 15:58:18 +0200

    konacna verzija koda

--- script.js
+++ script.js
@@ -39,74 +39,74 @@ const deleteCARS = (id) => {
   }
   
 
-  const addCar = () => {
-    var CarFormID=document.getElementById("car-id").value;
-    var CarFormName=document.getElementById("car-name").value;
-    var CarFormManufacturer=document.getElementById("car-manufacturer").value;
-    var CarFormURL=document.getElementById("car-url").value;
-    var CarFormPrice=document.getElementById("car-price").value;
-    var CarFormYear=document.getElementById("car-year").value;
+const addCar = () => {
+  var CarFormID=document.getElementById("car-id").value;
+  var CarFormName=document.getElementById("car-name").value;
+  var CarFormManufacturer=document.getElementById("car-manufacturer").value;
+  var CarFormURL=document.getElementById("car-url").value;
+  var CarFormPrice=document.getElementById("car-price").value;
+  var CarFormYear=document.getElementById("car-year").value;
       
-    fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars`, {
-      method: 'POST',
-      headers: new Headers({'content-type': 'application/json'}),
-      body: JSON.stringify({
+  fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars`, {
+    method: 'POST',
+    headers: new Headers({'content-type': 'application/json'}),
+    body: JSON.stringify({
   
-        "id": CarFormID,
-        "name": CarFormName,
-        "manufacturer": CarFormManufacturer,
-        "imageUrl": CarFormURL,
-        "price": CarFormPrice,
-        "year": CarFormYear
+      "id": CarFormID,
+      "name": CarFormName,
+      "manufacturer": CarFormManufacturer,
+      "imageUrl": CarFormURL,
+      "price": CarFormPrice,
+      "year": CarFormYear
 
       })
     })
-    .then(res => {
-      console.log(res);
-    })
-  }
+  .then(res => {
+    console.log(res);
+  })
+}
 
-  const editData = (carID) => {
-    const car=cars.find(car => car.id === carID)
-    const CarFormID1=document.getElementById("car-id1");
-    const CarFormName1=document.getElementById("car-name1");
-    const CarFormManufacturer1=document.getElementById("car-manufacturer1");
-    const CarFormURL1=document.getElementById("car-url1");
-    const CarFormPrice1=document.getElementById("car-price1");
-    const CarFormYear1=document.getElementById("car-year1");
+const editData = (carID) => {
+  const car=cars.find(car => car.id === carID)
+  const CarFormID1=document.getElementById("car-id1");
+  const CarFormName1=document.getElementById("car-name1");
+  const CarFormManufacturer1=document.getElementById("car-manufacturer1");
+  const CarFormURL1=document.getElementById("car-url1");
+  const CarFormPrice1=document.getElementById("car-price1");
+  const CarFormYear1=document.getElementById("car-year1");
       
-    CarFormID1.value=car.id;
-    CarFormName1.value=car.name;
-    CarFormManufacturer1.value=car.manufacturer;
-    CarFormURL1.value=car.imageUrl;
-    CarFormPrice1.value=car.price;
-    CarFormYear1.value=car.year;
+  CarFormID1.value=car.id;
+  CarFormName1.value=car.name;
+  CarFormManufacturer1.value=car.manufacturer;
+  CarFormURL1.value=car.imageUrl;
+  CarFormPrice1.value=car.price;
+  CarFormYear1.value=car.year;
   }
   
-  const putCar=()=>{
-    CarFormID1=document.getElementById("car-id1").value;
-    CarFormName1=document.getElementById("car-name1").value;
-    CarFormManufacturer1=document.getElementById("car-manufacturer1").value;
-    CarFormURL1=document.getElementById("car-url1").value;
-    CarFormPrice1=document.getElementById("car-price1").value;
-    CarFormYear1=document.getElementById("car-year1").value;
+const putCar=()=>{
+  CarFormID1=document.getElementById("car-id1").value;
+  CarFormName1=document.getElementById("car-name1").value;
+  CarFormManufacturer1=document.getElementById("car-manufacturer1").value;
+  CarFormURL1=document.getElementById("car-url1").value;
+  CarFormPrice1=document.getElementById("car-price1").value;
+  CarFormYear1=document.getElementById("car-year1").value;
 
   
-    fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars', {
-      method: 'PUT',
-      headers: new Headers({'content-type': 'application/json'}),
-      body: JSON.stringify({
+  fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars', {
+    method: 'PUT',
+    headers: new Headers({'content-type': 'application/json'}),
+    body: JSON.stringify({
   
-        "id": CarFormID1,
-        "name": CarFormName1,
-        "manufacturer": CarFormManufacturer1,
-        "imageUrl": CarFormURL1,
-        "price": CarFormPrice1,
-        "year": CarFormYear1
+      "id": CarFormID1,
+      "name": CarFormName1,
+      "manufacturer": CarFormManufacturer1,
+      "imageUrl": CarFormURL1,
+      "price": CarFormPrice1,
+      "year": CarFormYear1
   
-      })
     })
-    .then(res => {
-      console.log(res);
-    })
-  }
+  })
+  .then(res => {
+    console.log(res);
+  })
+}
